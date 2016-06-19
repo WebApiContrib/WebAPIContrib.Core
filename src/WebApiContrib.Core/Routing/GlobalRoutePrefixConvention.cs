f3a1f9a7ddb4389ec3a52e92fbaa77801f5460ad
filipw <wojcieszyn@gmail.com>
2016-06-19 09:02:32 +0200

    added GlobalRoutePrefixConvention

---
+++ src/WebApiContrib.Core/Routing/GlobalRoutePrefixConvention.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Routing;
+
+namespace WebApiContrib.Core.Routing
+{
+    public class GlobalRoutePrefixConvention : IApplicationModelConvention
+    {
+        private readonly AttributeRouteModel _centralPrefix;
+
+        public GlobalRoutePrefixConvention(IRouteTemplateProvider routeTemplateProvider)
+        {
+            _centralPrefix = new AttributeRouteModel(routeTemplateProvider);
+        }
+
+        public void Apply(ApplicationModel application)
+        {
+            foreach (var controller in application.Controllers)
+            {
+                var matchedSelectors = controller.Selectors.Where(x => x.AttributeRouteModel != null).ToList();
+                if (matchedSelectors.Any())
+                {
+                    foreach (var selectorModel in matchedSelectors)
+                    {
+                        selectorModel.AttributeRouteModel = AttributeRouteModel.CombineAttributeRouteModel(_centralPrefix,
+                            selectorModel.AttributeRouteModel);
+                    }
+                }
+
+                var unmatchedSelectors = controller.Selectors.Where(x => x.AttributeRouteModel == null).ToList();
+                if (unmatchedSelectors.Any())
+                {
+                    foreach (var selectorModel in unmatchedSelectors)
+                    {
+                        selectorModel.AttributeRouteModel = _centralPrefix;
+                    }
+                }
+            }
+        }
+    }
+}
