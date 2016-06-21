f2c8684d80c16e6ff045df8c956ca1a267dc012e
filipw <wojcieszyn@gmail.com>
2016-06-21 15:30:41 +0200

    added PlainTextFormatter feature

---
+++ WebApiContrib.Core.Formatter.PlainText/PlainTextFormatterOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApiContrib.Core.Formatter.PlainText
+{
+    public class PlainTextFormatterOptions
+    {
+        public Encoding[] SupportedEncodings { get; set; } = { Encoding.UTF8 };
+
+        public string[] SupportedMediaTypes { get; set; } = { "text/plain" };
+    }
+}
---
+++ WebApiContrib.Core.Formatter.PlainText/PlainTextOutputFormatter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Net.Http.Headers;
+
+namespace WebApiContrib.Core.Formatter.PlainText
+{
+    public class PlainTextOutputFormatter : StringOutputFormatter
+    {
+        public PlainTextOutputFormatter(PlainTextFormatterOptions opts)
+        {
+            foreach (var enc in opts.SupportedEncodings)
+            {
+                SupportedEncodings.Add(enc);
+            }
+
+            foreach (var mediaType in opts.SupportedMediaTypes)
+            {
+                SupportedMediaTypes.Add(MediaTypeHeaderValue.Parse(mediaType));
+            }
+        }
+
+        public PlainTextOutputFormatter() : this(new PlainTextFormatterOptions())
+        {
+        }
+
+        public override bool CanWriteResult(OutputFormatterCanWriteContext context)
+        {
+            // this override is necessary to stop StringOutputFormatter from injecting UTF-8
+            // and text/plain media type arbitrarily
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            return context.ObjectType == typeof(string) || context.Object is string;
+        }
+    }
+}
