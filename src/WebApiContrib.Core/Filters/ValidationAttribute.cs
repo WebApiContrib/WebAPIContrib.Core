9f256e16fcbebe6ec6af1c8b6046020fc8393816
filipw <wojcieszyn@gmail.com>
2016-06-22 10:13:34 +0200

    added ValidationAttribute from old WebApiContrib

---
+++ src/WebApiContrib.Core/Filters/ValidationAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace WebApiContrib.Core.Filters
+{
+    public class ValidationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext actionContext)
+        {
+            if (!actionContext.ModelState.IsValid)
+            {
+                var errors = actionContext.ModelState
+                    .Where(e => e.Value.Errors.Count > 0)
+                    .Select(e => new
+                    {
+                        Name = e.Key,
+                        Message = e.Value.Errors.First().ErrorMessage
+                    }).ToArray();
+
+                actionContext.Result = new BadRequestObjectResult(errors);
+            }
+        }
+    }
+}
