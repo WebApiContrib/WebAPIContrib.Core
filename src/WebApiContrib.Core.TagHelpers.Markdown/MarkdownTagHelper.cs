9cc51a6ca540c149b354554c7814867dd98943ab
filipw <wojcieszyn@gmail.com>
2016-06-22 21:23:01 +0200

    added Markdown project

---
+++ src/WebApiContrib.Core.TagHelpers.Markdown/MarkdownTagHelper.cs
@@ -0,0 +1,46 @@
+﻿using Markdig;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApiContrib.Core.TagHelpers.Markdown
+{
+    [HtmlTargetElement("md")]
+    public class MarkdownTagHelper : MarkdownTagHelperBase
+    {
+        public bool PipeTables { get; set; }
+
+        public bool GridTables { get; set; }
+
+        public bool ExtraEmphasis { get; set; }
+
+        public bool DefinitionLists { get; set; }
+
+        public bool Footnotes { get; set; }
+
+        public bool TaskLists { get; set; }
+
+        public bool ExtraBulletLists { get; set; }
+
+        public bool Abbreviations { get; set; }
+
+        public bool Emoji { get; set; }
+
+        public override MarkdownPipeline GetPipeline()
+        {
+            var pipelineBuilder = new MarkdownPipelineBuilder();
+
+            if (PipeTables) pipelineBuilder = pipelineBuilder.UsePipeTables();
+            if (GridTables) pipelineBuilder = pipelineBuilder.UseGridTables();
+            if (ExtraEmphasis) pipelineBuilder = pipelineBuilder.UseEmphasisExtras();
+            if (DefinitionLists) pipelineBuilder = pipelineBuilder.UseDefinitionLists();
+            if (Footnotes) pipelineBuilder = pipelineBuilder.UseFootnotes();
+            if (TaskLists) pipelineBuilder = pipelineBuilder.UseTaskLists();
+            if (ExtraBulletLists) pipelineBuilder = pipelineBuilder.UseListExtras();
+            if (Abbreviations) pipelineBuilder = pipelineBuilder.UseAbbreviations();
+            if (Emoji) pipelineBuilder = pipelineBuilder.UseEmojiAndSmiley();
+
+            return pipelineBuilder.Build();
+        }
+
+        public override string Name => "md";
+    }
+}
---
+++ src/WebApiContrib.Core.TagHelpers.Markdown/MarkdownTagHelperBase.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using Markdig;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebApiContrib.Core.TagHelpers.Markdown
+{
+    public abstract class MarkdownTagHelperBase : TagHelper
+    {
+        public abstract MarkdownPipeline GetPipeline();
+
+        public abstract string Name { get; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var pipeline = GetPipeline();
+
+            // get rid of the surrounding tag
+            output.TagName = output.TagName != Name ? output.TagName : null;
+            var content = await output.GetChildContentAsync();
+            var markdown = content.GetContent();
+
+            if (!string.IsNullOrWhiteSpace(markdown))
+            {
+                var html = Markdig.Markdown.ToHtml(markdown, pipeline);
+                output.Content.SetHtmlContent(html);
+            }
+            else
+            {
+                output.Content.SetHtmlContent(string.Empty);
+            }
+        }
+    }
+}
