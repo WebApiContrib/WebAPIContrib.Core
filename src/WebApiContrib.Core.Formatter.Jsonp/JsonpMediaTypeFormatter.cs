74a1e31bf5b4921b84783a7fce7d6ee4356993f5
filipw <wojcieszyn@gmail.com>
2016-07-01 10:10:32 +0200

    clean up and improve JSONP code

--- src/WebApiContrib.Core.Formatter.Jsonp/JsonpMediaTypeFormatter.cs
+++ src/WebApiContrib.Core.Formatter.Jsonp/JsonpMediaTypeFormatter.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Linq;
-using System.Net;
 using System.Text;
-using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Formatters;
@@ -11,15 +9,15 @@ using Microsoft.Net.Http.Headers;
 
 namespace WebApiContrib.Core.Formatter.Jsonp
 {
-    public class JsonpMediaTypeFormatter : OutputFormatter
+    public class JsonpMediaTypeFormatter : TextOutputFormatter
     {
         private static readonly MediaTypeHeaderValue _applicationJavaScript = new MediaTypeHeaderValue("application/javascript");
         private static readonly MediaTypeHeaderValue _applicationJsonp = new MediaTypeHeaderValue("application/json-p");
         private static readonly MediaTypeHeaderValue _textJavaScript = new MediaTypeHeaderValue("text/javascript");
-        private readonly OutputFormatter _jsonMediaTypeFormatter;
+        private readonly JsonOutputFormatter _jsonMediaTypeFormatter;
         private readonly string _callbackQueryParameter;
 
-        public JsonpMediaTypeFormatter(OutputFormatter jsonMediaTypeFormatter, string callbackQueryParameter = null)
+        public JsonpMediaTypeFormatter(JsonOutputFormatter jsonMediaTypeFormatter, string callbackQueryParameter = null)
         {
             if (jsonMediaTypeFormatter == null)
             {
@@ -32,12 +30,10 @@ namespace WebApiContrib.Core.Formatter.Jsonp
             SupportedMediaTypes.Add(_textJavaScript);
             SupportedMediaTypes.Add(_applicationJavaScript);
             SupportedMediaTypes.Add(_applicationJsonp);
-            //foreach (var encoding in _jsonMediaTypeFormatter.SupportedMediaTypes)
-            //{
-            //    SupportedEncodings.Add(encoding);
-            //}
-
-            //MediaTypeMappings.Add(new JsonpQueryStringMapping(_callbackQueryParameter, _textJavaScript));
+            foreach (var encoding in _jsonMediaTypeFormatter.SupportedEncodings)
+            {
+                SupportedEncodings.Add(encoding);
+            }
         }
 
         public override bool CanWriteResult(OutputFormatterCanWriteContext context)
@@ -50,7 +46,7 @@ namespace WebApiContrib.Core.Formatter.Jsonp
             return _jsonMediaTypeFormatter.CanWriteResult(context);
         }
 
-        public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context)
+        public override async Task WriteResponseBodyAsync(OutputFormatterWriteContext context, Encoding selectedEncoding)
         {
             if (context == null)
             {
@@ -65,20 +61,20 @@ namespace WebApiContrib.Core.Formatter.Jsonp
                     throw new InvalidOperationException($"Callback '{callback}' is invalid!");
                 }
 
-                using (var writer = new StreamWriter(context.HttpContext.Response.Body, Encoding.UTF8, bufferSize: 4096, leaveOpen: true))
+                using (var writer = context.WriterFactory(context.HttpContext.Response.Body, selectedEncoding))
                 {
                     // the /**/ is a specific security mitigation for "Rosetta Flash JSONP abuse"
                     // the typeof check is just to reduce client error noise
                     writer.Write("/**/ typeof " + callback + " === 'function' && " + callback + "(");
                     writer.Flush();
-                    await _jsonMediaTypeFormatter.WriteResponseBodyAsync(context);
+                    _jsonMediaTypeFormatter.WriteObject(writer, context.Object);
                     writer.Write(");");
-                    writer.Flush();
+                    await writer.FlushAsync();
                 }
             }
             else
             {
-                throw new InvalidOperationException("Not a valid JSONP request.");
+                await _jsonMediaTypeFormatter.WriteResponseBodyAsync(context, selectedEncoding);
             }
         }
 
