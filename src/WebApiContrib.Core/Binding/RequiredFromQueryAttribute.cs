65f6ca84f13ff7b805fc493338c3e30bb75e5365
filipw <wojcieszyn@gmail.com>
2016-09-19 13:36:41 +0200

    added RequiredFromQueryAttribute and RequiredFromQueryActionConstraint

---
+++ src/WebApiContrib.Core/Binding/RequiredFromQueryAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using WebApiContrib.Core.Constraints;
+
+namespace WebApiContrib.Core.Binding
+{
+    public class RequiredFromQueryAttribute : FromQueryAttribute, IParameterModelConvention
+    {
+        public void Apply(ParameterModel parameter)
+        {
+            if (parameter.Action.Selectors != null && parameter.Action.Selectors.Any())
+            {
+                parameter.Action.Selectors.Last().ActionConstraints.Add(new RequiredFromQueryActionConstraint(parameter.BindingInfo?.BinderModelName ?? parameter.ParameterName));
+            }
+        }
+    }
+}
---
+++ src/WebApiContrib.Core/Constraints/RequiredFromQueryActionConstraint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ActionConstraints;
+
+namespace WebApiContrib.Core.Constraints
+{
+    public class RequiredFromQueryActionConstraint : IActionConstraint
+    {
+        private readonly string _parameter;
+
+        public RequiredFromQueryActionConstraint(string parameter)
+        {
+            _parameter = parameter;
+        }
+
+        public int Order => 999;
+
+        public bool Accept(ActionConstraintContext context)
+        {
+            for (var i = 0; i < _parameter.Length; i++)
+            {
+                if (!context.RouteContext.HttpContext.Request.Query.ContainsKey(_parameter))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
