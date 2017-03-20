7849c09e4c6a67aaf52f605026218673138707df
filipw <wojcieszyn@gmail.com>
2017-03-20 16:53:55 +0100

    added RegisterForDispose

---
+++ src/WebApiContrib.Core/HttpContextExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+
+namespace WebApiContrib.Core
+{
+    public static class HttpContextExtensions
+    {
+        public static T RegisterForDispose<T>(this T disposable, HttpContext context) where T : IDisposable
+        {
+            context.Response.RegisterForDispose(disposable);
+            return disposable;
+        }
+    }
+}
