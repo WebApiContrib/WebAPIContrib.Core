7711964dc7e74156455e1aacefc23790e5f80b11
filipw <wojcieszyn@gmail.com>
2017-06-22 16:04:41 +0200

    added null checks to ValidationAttribute

--- src/WebApiContrib.Core/Filters/ValidationAttribute.cs
+++ src/WebApiContrib.Core/Filters/ValidationAttribute.cs
@@ -1,18 +1,39 @@
 ﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
+using System.Collections.Generic;
 
 namespace WebApiContrib.Core.Filters
 {
     public class ValidationAttribute : ActionFilterAttribute
     {
+        public bool AllowNull { get; }
+
         public override void OnActionExecuting(ActionExecutingContext actionContext)
         {
+            if (!AllowNull)
+            {
+                var nullArguments = actionContext.ActionArguments.Where(x => x.Value == null);
+                if (nullArguments.Any())
+                {
+                    var errors = new List<Error>();
+                    foreach(var nullArgument in nullArguments)
+                    {
+                        errors.Add(new Error
+                        {
+                            Name = nullArgument.Key,
+                            Message = "Value cannot be null."
+                        });
+                    }
+                    actionContext.Result = new BadRequestObjectResult(errors);
+                }
+            }
+
             if (!actionContext.ModelState.IsValid)
             {
                 var errors = actionContext.ModelState
                     .Where(e => e.Value.Errors.Count > 0)
-                    .Select(e => new
+                    .Select(e => new Error
                     {
                         Name = e.Key,
                         Message = e.Value.Errors.First().ErrorMessage
@@ -20,6 +41,13 @@ namespace WebApiContrib.Core.Filters
 
                 actionContext.Result = new BadRequestObjectResult(errors);
             }
+        }
+
+        private class Error
+        {
+            public string Name { get; set; }
+
+            public string Message { get; set; }
         }
     }
 }
