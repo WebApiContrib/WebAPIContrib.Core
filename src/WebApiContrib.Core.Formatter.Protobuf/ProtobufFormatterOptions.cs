712a3c8ff16a8bae823cef37150dfca53df3c8a4
damienbod <damien_bod@hotmail.com>
2017-06-29 16:26:24 +0200

    removing comments, code clean up

--- src/WebApiContrib.Core.Formatter.Protobuf/ProtobufFormatterOptions.cs
+++ src/WebApiContrib.Core.Formatter.Protobuf/ProtobufFormatterOptions.cs
@@ -5,8 +5,6 @@ namespace WebApiContrib.Core.Formatter.Protobuf
 {
     public class ProtobufFormatterOptions
     {
-        //public IFormatterResolver FormatterResolver { get; set; } = ContractlessStandardResolver.Instance;
-
         public HashSet<string> SupportedContentTypes { get; set; } = new HashSet<string> { "application/x-protobuf", "application/x-protobuf" };
 
         public HashSet<string> SupportedExtensions { get; set; } = new HashSet<string> { "proto" };
