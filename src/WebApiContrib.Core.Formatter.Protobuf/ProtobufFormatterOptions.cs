abe7b47f658e59c39f65215b3f43eb64c2fa3e6c
damienbod <damien_bod@hotmail.com>
2017-06-29 17:06:43 +0200

    adding tests for content types

--- src/WebApiContrib.Core.Formatter.Protobuf/ProtobufFormatterOptions.cs
+++ src/WebApiContrib.Core.Formatter.Protobuf/ProtobufFormatterOptions.cs
@@ -5,7 +5,7 @@ namespace WebApiContrib.Core.Formatter.Protobuf
 {
     public class ProtobufFormatterOptions
     {
-        public HashSet<string> SupportedContentTypes { get; set; } = new HashSet<string> { "application/x-protobuf", "application/x-protobuf" };
+        public HashSet<string> SupportedContentTypes { get; set; } = new HashSet<string> { "application/x-protobuf", "application/protobuf", "application/x-google-protobuf" };
 
         public HashSet<string> SupportedExtensions { get; set; } = new HashSet<string> { "proto" };
 
--- tests/WebApiContrib.Core.Protobuf.Tests/ProtobufTests.cs
+++ tests/WebApiContrib.Core.Protobuf.Tests/ProtobufTests.cs
@@ -22,7 +22,7 @@ namespace WebApiContrib.Core.Protobuf.Tests
         }
 
         [Fact]
-        public async Task GetCollection_Protobuf_Header()
+        public async Task GetCollection_X_Protobuf_Header()
         {
             var client = _server.CreateClient();
 
@@ -39,7 +39,56 @@ namespace WebApiContrib.Core.Protobuf.Tests
         }
 
         [Fact]
+        public async Task GetCollection_Protobuf_Header()
+        {
+            var client = _server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/books");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/protobuf"));
+            var result = await client.SendAsync(request);
+            var books = ProtoBuf.Serializer.Deserialize<Book[]>(await result.Content.ReadAsStreamAsync());
+
+            Assert.Equal(2, books.Length);
+            Assert.Equal(Book.Data[0].Author, books[0].Author);
+            Assert.Equal(Book.Data[0].Title, books[0].Title);
+            Assert.Equal(Book.Data[1].Author, books[1].Author);
+            Assert.Equal(Book.Data[1].Title, books[1].Title);
+        }
+
+        [Fact]
+        public async Task GetCollection_X_Google_Protobuf_Header()
+        {
+            var client = _server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/books");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-google-protobuf"));
+            var result = await client.SendAsync(request);
+            var books = ProtoBuf.Serializer.Deserialize<Book[]>(await result.Content.ReadAsStreamAsync());
+
+            Assert.Equal(2, books.Length);
+            Assert.Equal(Book.Data[0].Author, books[0].Author);
+            Assert.Equal(Book.Data[0].Title, books[0].Title);
+            Assert.Equal(Book.Data[1].Author, books[1].Author);
+            Assert.Equal(Book.Data[1].Title, books[1].Title);
+        }
+
+        [Fact]
         public async Task GetById_Protobuf_Header()
+        {
+            var client = _server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "/api/books/1");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/protobuf"));
+            var result = await client.SendAsync(request);
+            var book = ProtoBuf.Serializer.Deserialize<Book>(await result.Content.ReadAsStreamAsync());
+
+            Assert.NotNull(book);
+            Assert.Equal(Book.Data[0].Author, book.Author);
+            Assert.Equal(Book.Data[0].Title, book.Title);
+        }
+
+        [Fact]
+        public async Task GetById_X_Protobuf_Header()
         {
             var client = _server.CreateClient();
 
@@ -54,7 +103,7 @@ namespace WebApiContrib.Core.Protobuf.Tests
         }
 
         [Fact]
-        public async Task Post_Protobuf_Header()
+        public async Task Post_X_Protobuf_Header()
         {
             var client = _server.CreateClient();
 
@@ -83,6 +132,63 @@ namespace WebApiContrib.Core.Protobuf.Tests
         }
 
         [Fact]
+        public async Task Post_Protobuf_Header()
+        {
+            var client = _server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "/api/books");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/protobuf"));
+
+            var book = new Book
+            {
+                Author = "Tim Parks",
+                Title = "Italian Ways: On and off the Rails from Milan to Palermo"
+            };
+
+            MemoryStream stream = new MemoryStream();
+            ProtoBuf.Serializer.Serialize<Book>(stream, book);
+
+            HttpContent data = new StreamContent(stream);
+
+            request.Content = data;
+            var result = await client.SendAsync(request);
+
+            var echo = ProtoBuf.Serializer.Deserialize<Book>(await result.Content.ReadAsStreamAsync());
+
+            Assert.NotNull(book);
+            Assert.Equal(book.Author, echo.Author);
+            Assert.Equal(book.Title, echo.Title);
+        }
+
+        [Fact]
+        public async Task Post_X_Google_Protobuf_Header()
+        {
+            var client = _server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Post, "/api/books");
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/x-google-protobuf"));
+
+            var book = new Book
+            {
+                Author = "Tim Parks",
+                Title = "Italian Ways: On and off the Rails from Milan to Palermo"
+            };
+
+            MemoryStream stream = new MemoryStream();
+            ProtoBuf.Serializer.Serialize<Book>(stream, book);
+
+            HttpContent data = new StreamContent(stream);
+
+            request.Content = data;
+            var result = await client.SendAsync(request);
+
+            var echo = ProtoBuf.Serializer.Deserialize<Book>(await result.Content.ReadAsStreamAsync());
+
+            Assert.NotNull(book);
+            Assert.Equal(book.Author, echo.Author);
+            Assert.Equal(book.Title, echo.Title);
+        }
+        [Fact]
         public async Task GetCollection_JSON_Header()
         {
             var client = _server.CreateClient();
@@ -97,46 +203,6 @@ namespace WebApiContrib.Core.Protobuf.Tests
             Assert.Equal(Book.Data[0].Title, books[0].Title);
             Assert.Equal(Book.Data[1].Author, books[1].Author);
             Assert.Equal(Book.Data[1].Title, books[1].Title);
-        }
-
-        [Fact]
-        public async Task GetById_JSON_Header()
-        {
-            var client = _server.CreateClient();
-
-            var request = new HttpRequestMessage(HttpMethod.Get, "/api/books/1");
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            var result = await client.SendAsync(request);
-
-            var book = JsonConvert.DeserializeObject<Book>(await result.Content.ReadAsStringAsync());
-
-            Assert.NotNull(book);
-            Assert.Equal(Book.Data[0].Author, book.Author);
-            Assert.Equal(Book.Data[0].Title, book.Title);
-        }
-
-        [Fact]
-        public async Task Post_JSON_Header()
-        {
-            var client = _server.CreateClient();
-
-            var request = new HttpRequestMessage(HttpMethod.Post, "/api/books");
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-            var book = new Book
-            {
-                Author = "Tim Parks",
-                Title = "Italian Ways: On and off the Rails from Milan to Palermo"
-            };
-
-            request.Content = new StringContent(JsonConvert.SerializeObject(book));
-            var result = await client.SendAsync(request);
-
-            var echo = JsonConvert.DeserializeObject<Book>(await result.Content.ReadAsStringAsync());
-
-            Assert.NotNull(book);
-            Assert.Equal(book.Author, echo.Author);
-            Assert.Equal(book.Title, echo.Title);
         }
     }
 }
