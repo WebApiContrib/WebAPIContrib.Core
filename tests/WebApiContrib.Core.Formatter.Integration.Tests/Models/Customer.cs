422d7e77dab2572985a03440ebadbe85ff960294
filipw <wojcieszyn@gmail.com>
2017-07-03 10:44:27 +0200

    removed unneeded code

--- tests/WebApiContrib.Core.Formatter.Integration.Tests/Models/Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApiContrib.Core.Formatter.Integration.Tests.Models
-{
-    public class Customer
-    {
-        public string CustomerId { get; set; }
-
-        public string FirstName { get; set; }
-    }
-}
