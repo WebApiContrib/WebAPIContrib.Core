2d4daa219c486c7b2ac2f87ec5a55fed2a884982
damienbod <damien_bod@hotmail.com>
2017-09-10 10:19:43 +0200

    Updating to dotnet 2.0

--- samples/WebApiContrib.Core.Samples/Program.cs
+++ samples/WebApiContrib.Core.Samples/Program.cs
@@ -1,6 +1,12 @@
-﻿using System.IO;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace WebApiContrib.Core.Samples
 {
@@ -8,14 +14,12 @@ namespace WebApiContrib.Core.Samples
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
-        }
     }
 }
