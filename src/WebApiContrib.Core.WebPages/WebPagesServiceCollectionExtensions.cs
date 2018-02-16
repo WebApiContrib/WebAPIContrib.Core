50d9777c86d74dec5c10ab80a9fedfe48521cd6f
filipw <wojcieszyn@gmail.com>
2018-02-16 11:52:53 +0100

    updated everything to .NET Standard 2.0

--- src/WebApiContrib.Core.WebPages/WebPagesServiceCollectionExtensions.cs
+++ src/WebApiContrib.Core.WebPages/WebPagesServiceCollectionExtensions.cs
@@ -1,14 +1,7 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc.Razor;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Extensions.PlatformAbstractions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace WebApiContrib.Core.WebPages
 {
