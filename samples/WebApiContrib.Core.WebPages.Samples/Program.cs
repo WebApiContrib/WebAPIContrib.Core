14de90e9109181a7e9847770a3b7e516ad5b1d88
damienbod <damien_bod@hotmail.com>
2018-09-14 09:40:47 +0200

    updating to .NET Core 2.1

--- samples/WebApiContrib.Core.WebPages.Samples/Program.cs
+++ samples/WebApiContrib.Core.WebPages.Samples/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.IO;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 
 namespace WebApiContrib.Core.WebPages.Samples
@@ -7,14 +7,11 @@ namespace WebApiContrib.Core.WebPages.Samples
     {
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>();
     }
 }
