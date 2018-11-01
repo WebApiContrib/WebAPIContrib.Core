70a5330acf589ebc000cc2ab34055320ffc2bd98
Victorio Berra <toryberra+github@gmail.com>
2018-11-01 12:38:55 -0500

    Make ReadStream public.
    I need to manually call `ReadStream` in my unit tests to convert a CSV stream back into a type so I can assert values and whatnot. Currently I am trying to wrap the `CsvInputFormatter` in a [RestEase](https://github.com/canton7/RestEase/) `ResponseDeserializer`.

--- src/WebApiContrib.Core.Formatter.Csv/CsvInputFormatter.cs
+++ src/WebApiContrib.Core.Formatter.Csv/CsvInputFormatter.cs
@@ -66,7 +66,7 @@ namespace WebApiContrib.Core.Formatter.Csv
             return false;
         }
 
-        private object ReadStream(Type type, Stream stream)
+        public object ReadStream(Type type, Stream stream)
         {
             Type itemType;
             var typeIsArray = false;
