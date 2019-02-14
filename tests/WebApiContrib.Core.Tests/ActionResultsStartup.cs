0aef6dcdd9f0d5bc9ae3a62f279781e1c53779d1
filipw <wojcieszyn@gmail.com>
2019-02-14 15:24:11 +0100

    added first ActionResults tests

---
+++ tests/WebApiContrib.Core.Tests/ActionResultsStartup.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http;
+using WebApiContrib.Core.Results;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiContrib.Core.Tests
+{
+    public class TestController : ControllerBase
+    {
+        [HttpGet("test-controller", Name = "TestController")]
+        public IActionResult Get()
+        {
+            return Ok("test controller");
+        }
+    } 
+
+    public class Item
+    {
+        public string Name { get; set; }
+    }
+
+    public class ActionResultsStartup
+    {
+        public IConfigurationRoot Configuration { get; }
+
+        public ActionResultsStartup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvcCore().AddJsonFormatters();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Map("/ok", b =>
+            {
+                b.Use(async (ctx, next) =>
+                {
+                    await ctx.Ok();
+                });
+            });
+
+            app.Map("/ok-with-object", b =>
+            {
+                b.Use(async (ctx, next) =>
+                {
+                    await ctx.Ok(new Item { Name = "test" });
+                });
+            });
+
+            app.Map("/accepted", b =>
+            {
+                b.Use(async (ctx, next) =>
+                {
+                    await ctx.Accepted();
+                });
+            });
+
+            app.Map("/accepted-with-object", b =>
+            {
+                b.Use(async (ctx, next) =>
+                {
+                    await ctx.Accepted(new Item { Name = "test" });
+                });
+            });
+
+            app.Map("/redirect-to-route", b =>
+            {
+                b.Use(async (ctx, next) =>
+                {
+                    await ctx.RedirectToRoute("TestController");
+                });
+            });
+
+            app.Map("/redirect-to-route-permanent", b =>
+            {
+                b.Use(async (ctx, next) =>
+                {
+                    await ctx.RedirectToRoutePermanent("TestController");
+                });
+            });
+
+            app.UseMvc();
+        }
+    }
+}
---
+++ tests/WebApiContrib.Core.Tests/ActionResultsTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace WebApiContrib.Core.Tests
+{
+    public class ActionResultsTests
+    {
+        private TestServer _server;
+
+        public ActionResultsTests()
+        {
+            _server = new TestServer(new WebHostBuilder()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseStartup<ActionResultsStartup>());
+        }
+
+        [Fact]
+        public async Task Ok()
+        {
+            var client = _server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/ok");
+            var result = await client.SendAsync(request);
+
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+        }
+
+        [Fact]
+        public async Task OkWithContent()
+        {
+            var client = _server.CreateClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, $"/ok-with-object");
+            var result = await client.SendAsync(request);
+            var objectResult = JsonConvert.DeserializeObject<Item>(await result.Content.ReadAsStringAsync());
+
+            Assert.Equal(HttpStatusCode.OK, result.StatusCode);
+            Assert.Equal("test", objectResult.Name);
+        }
+    }
+}
