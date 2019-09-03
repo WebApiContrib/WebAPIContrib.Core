442f6adbd560a02728dc8755859aaa3f64f035ba
James Sherburn <j.sherburn95@gmail.com>
2019-09-03 11:30:30 +0100

    Added a list of surrogate types to the ProtobufFormatterOptions to be applied in the input formatter.

--- src/WebApiContrib.Core.Formatter.Protobuf/ProtobufFormatterOptions.cs
+++ src/WebApiContrib.Core.Formatter.Protobuf/ProtobufFormatterOptions.cs
@@ -1,14 +1,24 @@
 ﻿
+using System;
 using System.Collections.Generic;
 
 namespace WebApiContrib.Core.Formatter.Protobuf
 {
     public class ProtobufFormatterOptions
     {
+        private readonly Dictionary<Type, Type> _surrogates = new Dictionary<Type, Type>();
+
         public HashSet<string> SupportedContentTypes { get; set; } = new HashSet<string> { "application/x-protobuf", "application/protobuf", "application/x-google-protobuf" };
 
         public HashSet<string> SupportedExtensions { get; set; } = new HashSet<string> { "proto" };
 
+        public IReadOnlyDictionary<Type, Type> Surrogates => _surrogates;
+
        public bool SuppressReadBuffering { get; set; } = false;
+
+       public void AddSurrogate(Type modelType, Type surrogateType)
+       {
+           _surrogates.Add(modelType, surrogateType);
+       }
     }
 }
